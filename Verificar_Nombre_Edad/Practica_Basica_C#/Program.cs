bdaa7961689e68b09e32b312a9da8ca1db07c04d
TricksterRD <ruizsanchezyael@gmail.com>
2024-05-19 18:29:58 -0400

    Agregar archivos de proyecto.

---
+++ Verificar_Nombre_Edad/Practica_Basica_C#/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Basica_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nombre");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su edad");
+            int edad = int.Parse(Console.ReadLine());
+
+            if (edad >= 18) Console.WriteLine("Usted es mayor de edad");
+            else Console.WriteLine("Usted es menor de edad");
+
+            for (int i = 1; i <= 10; i++) { Console.WriteLine(i); };
+
+            Console.ReadLine();
+        }
+    }
+}
